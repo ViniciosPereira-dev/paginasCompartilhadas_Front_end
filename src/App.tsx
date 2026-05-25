88e5e874dce6593e33ffe727e4ede9115dc00c5b
Vinicios Rosa <viniciosrp.dev@hotmail.com>
2026-05-25 16:11:39 -0300

    Setting up the environment | structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useEffect } from "react";
-import api from "./services/api";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-    useEffect(() => {
-
-        async function testRecommendation() {
-
-            try {
-
-                const response = await api.get("/recommendation");
-
-                console.log(response.data);
-
-            } catch (error) {
-
-                console.error(error);
-
-            }
-
-        }
-
-        testRecommendation();
-
-    }, []);
-
-    return <h1>Teste Frontend</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
